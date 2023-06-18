8292d043fb9a607d53adf60b429eed26d87372dd
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-06-18 11:32:13 -0700

    Create PropertyDetails.jsx

---
+++ src/routes/PropertyDetails.jsx
@@ -0,0 +1,11 @@
+import HouseDetails from "../components/PropertyDetails/HouseDetails";
+
+import { Container } from '@chakra-ui/react'
+const PropertyDetails = () => {
+  return (
+    <Container maxW='container.lg' px='6'>
+    <HouseDetails /></Container>
+  )
+}
+
+export default PropertyDetails
